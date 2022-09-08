6ad18b6dab535963bcf868b0f7bdb852a2209246
zhou tao <zhoutao@itiger.com>
2022-09-08 19:34:24 +0800

    pprof

---
+++ pprof/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+)
+
+func setup() {
+	// 这里面有一些初始化的操作
+}
+
+func main() {
+	setup()
+
+	// 用于监听服务退出
+	done := make(chan error, 2)
+	// 用于控制服务退出，传入同一个 stop，做到只要有一个服务退出了那么另外一个服务也会随之退出
+	stop := make(chan struct{}, 0)
+	// for debug
+	go func() {
+		done <- pprof(stop)
+	}()
+
+	// 主服务
+	go func() {
+		done <- app(stop)
+	}()
+
+	// stoped 用于判断当前 stop 的状态
+	var stoped bool
+	// 这里循环读取 done 这个 channel
+	// 只要有一个退出了，我们就关闭 stop channel
+	// len()可以用来查看数组或slice的长度，cap()可以用来查看数组或slice的容量
+	for i := 0; i < cap(done); i++ {
+		if err := <-done; err != nil {
+			log.Printf("server exit err: %+v", err)
+		}
+
+		if !stoped {
+			stoped = true
+			// close函数是一个内建函数， 用来关闭channel，这个channel要么是双向的， 要么是只写的（chan<- Type）。
+			// 这个方法应该只由发送者调用， 而不是接收者。
+			// 当最后一个发送的值都被接收者从关闭的channel(下简称为c)中接收时,
+			// 接下来所有接收的值都会非阻塞直接成功，返回channel元素的零值。
+			close(stop)
+		}
+	}
+}
+
+func app(stop <-chan struct{}) error {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
+
+	return server(mux, ":8080", stop)
+}
+
+func pprof(stop <-chan struct{}) error {
+	// 注意这里主要是为了模拟服务意外退出，用于验证一个服务退出，其他服务同时退出的场景
+	//go func() {
+	//  // DefaultServeMux 默认注册了/debug/pprof 路由
+	//	server(http.DefaultServeMux, ":8081", stop)
+	//}()
+	//
+	//time.Sleep(5 * time.Second)
+	//return fmt.Errorf("mock pprof exit")
+	return server(http.DefaultServeMux, ":8081", stop)
+}
+
+// 启动一个服务
+func server(handler http.Handler, addr string, stop <-chan struct{}) error {
+	s := http.Server{
+		Handler: handler,
+		Addr:    addr,
+	}
+
+	// 这个 goroutine 我们可以控制退出，因为只要 stop 这个 channel close 或者是写入数据，这里就会退出
+	// 同时因为调用了 s.Shutdown 调用之后，http 这个函数启动的 http server 也会优雅退出
+	go func() {
+		<-stop
+		log.Printf("server will exiting, addr: %s", addr)
+		s.Shutdown(context.Background())
+	}()
+
+	return s.ListenAndServe()
+}
